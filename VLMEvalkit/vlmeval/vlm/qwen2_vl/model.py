ba2b72e9a84148cd92103de8e9973e5aab9c2fa6
zl3466 <zl3466@nyu.edu>
2025-09-18 14:28:43 -0400

    adding custon model config

--- VLMEvalkit/vlmeval/vlm/qwen2_vl/model.py
+++ VLMEvalkit/vlmeval/vlm/qwen2_vl/model.py
@@ -116,18 +116,18 @@ class Qwen2VLChat(Qwen2VLPromptMixin, BaseModel):
         # If only one process and GPU memory is less than 40GB
         if '72b' in self.model_path.lower():
             self.model = MODEL_CLS.from_pretrained(
-                model_path, torch_dtype='auto', device_map=split_model(), attn_implementation='flash_attention_2'
+                model_path, torch_dtype=torch.bfloat16, device_map=split_model(), attn_implementation='flash_attention_2'
             )
             self.model.eval()
         elif auto_split_flag():
             assert world_size == 1, 'Only support world_size == 1 when AUTO_SPLIT is set for non-72B Qwen2-VL'
             # Will Use All GPUs to run one model
             self.model = MODEL_CLS.from_pretrained(
-                model_path, torch_dtype='auto', device_map='auto', attn_implementation='flash_attention_2'
+                model_path, torch_dtype=torch.bfloat16, device_map='auto', attn_implementation='flash_attention_2'
             )
         else:
             self.model = MODEL_CLS.from_pretrained(
-                model_path, torch_dtype='auto', device_map='cpu', attn_implementation='flash_attention_2'
+                model_path, torch_dtype=torch.bfloat16, device_map='cpu', attn_implementation='flash_attention_2'
             )
             self.model.cuda().eval()
 
