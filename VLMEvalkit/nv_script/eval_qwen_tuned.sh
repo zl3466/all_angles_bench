d9db05df50e47f1130fb07307052f7af80095883
zl3466 <zl3466@nyu.edu>
2025-09-19 14:48:27 -0400

    adding custon model config

--- VLMEvalkit/nv_script/eval_qwen_tuned.sh
+++ VLMEvalkit/nv_script/eval_qwen_tuned.sh
@@ -51,7 +51,8 @@ source /lustre/fsw/portfolios/nvr/users/ymingli/miniconda3/etc/profile.d/conda.s
 conda activate allanb
 export HF_HUB_CACHE="/lustre/fsw/portfolios/nvr/users/ymingli/cache/huggingface/hub"
 export OPENAI_API_KEY="$OPENAI_API_KEY"
-export THOUGHT_PROCESS=1
+export THOUGHT_PROCESS="1"
+echo "DEBUG: THOUGHT_PROCESS is set to: $THOUGHT_PROCESS"
 
 cd "/lustre/fsw/portfolios/nvr/users/ymingli/projects/playground/github/all_angles_bench/VLMEvalkit"
 
